40133e5270b635fea925ede5ff99645b0ac44d08
roprogramer91 <roprogramer91@gmail.com>
2025-01-10 13:35:36 -0300

    Agrega ruta de prueba para testeo

--- src/routes/farmacia.routes.js
+++ src/routes/farmacia.routes.js
@@ -5,4 +5,9 @@ const router = Router();
 
 router.get('/turno', getFarmaciaTurno);
 
+router.get('/test', (req, res) => {
+    res.json({ message: 'Ruta de prueba funcionando correctamente' });
+  });
+  
+
 module.exports = router;
