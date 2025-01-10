d9c0722018412aa2adb09270af5d30d18aa587df
roprogramer91 <roprogramer91@gmail.com>
2025-01-10 13:48:36 -0300

    se fusiona server con app para testeo

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
-const express = require ('express');
-const morgan = require ('morgan');
-const cors = require ('cors');
-require ('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
+// Inicializar app de Express
 const app = express();
 
-//Middlewares
+// Middlewares
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
-//Rutas
-app.use ('/api/farmacia', require('./routes/farmacia.routes.js'));
+// Rutas
+app.use('/api/farmacia', require('./routes/farmacia.routes'));
+
+// Inicializar servidor
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 
 module.exports = app;
