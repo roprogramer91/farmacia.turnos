bbdc1821fe3e89c9e02ebb8f3bf6b7898a0e6f11
roprogramer91 <roprogramer91@gmail.com>
2025-01-10 13:37:43 -0300

    se agregan logs de testeo

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const app = require('./app'); 
 const PORT = process.env.PORT || 3000;
 
+
+console.log('Iniciando servidor en producción...');
+
+
 app.listen (PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
