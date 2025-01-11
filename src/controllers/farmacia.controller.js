5132c21c73e8a525c587422faee70a01c75ccd57
roprogramer91 <roprogramer91@gmail.com>
2025-01-11 14:35:08 -0300

    debug  13 y 15

--- src/controllers/farmacia.controller.js
+++ src/controllers/farmacia.controller.js
@@ -9,7 +9,10 @@ const getFarmaciaTurno = async (req, res) => {
     }
 
     const farmaciaTurno = events[0].summary;
+    
+    console.log('Nombre de farmacia del calendario:', farmaciaTurno);
     const [result] = await pool.query('SELECT * FROM farmacias WHERE nombre = ?', [farmaciaTurno]);
+    console.log('Resultado de la consulta:', result);
 
     if (result.length > 0) {
       const farmacia = result[0];
